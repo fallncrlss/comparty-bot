1b69c681b20e59bb24ec8561512236b95e03cc12
fallncrlss <artemgricenko28@gmail.com>
2022-01-17 00:26:59 +0200

    :adhesive_bandage: Update user full name display (missing space)

--- src/domains/user/repositories/queries/user/fetch_top_users_by_rating.sql
+++ src/domains/user/repositories/queries/user/fetch_top_users_by_rating.sql
@@ -1,4 +1,4 @@
-SELECT CONCAT(first_name, last_name) AS "full_name!", COALESCE(SUM(r.amount), 0.00) AS "rating_amount!"
+SELECT CONCAT(first_name, ' ', last_name) AS "full_name!", COALESCE(SUM(r.amount), 0.00) AS "rating_amount!"
 FROM rating r
 INNER JOIN chat_user cu ON cu.chat_user_id = r.chat_user_id
 INNER JOIN "user" u ON u.user_id = cu.user_id
