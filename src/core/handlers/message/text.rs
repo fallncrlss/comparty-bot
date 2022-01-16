347b63209c1e20ef82f820e8741f302bab1fac54
fallncrlss <artemgricenko28@gmail.com>
2022-01-17 00:18:38 +0200

    :ambulance: Prevent panic during reply message user unwrap (rating trigger handler)

--- src/core/handlers/message/text.rs
+++ src/core/handlers/message/text.rs
@@ -39,7 +39,6 @@ pub async fn rating_trigger_handler(
     domain_holder: &injected::DomainHolder,
 ) -> Result<(), anyhow::Error> {
     let msg_text = cx.update.text().unwrap();
-    let user = cx.update.from().unwrap();
     let chat_id = cx.update.chat_id();
 
     let chat_settings = domain_holder
@@ -54,24 +53,20 @@ pub async fn rating_trigger_handler(
 
     if let Ok(rating_trigger) = lib::enums::RatingTriggers::from_str(msg_text) {
         if let Some(reply_msg) = cx.update.reply_to_message() {
-            if !reply_msg.from().unwrap().is_bot {
-            domain_holder
-                .user
-                .controller
-                .create_if_not_exists(user, chat_id)
-                .await?;
+            if let Some(reply_user) = reply_msg.from() {
+                if !reply_user.is_bot {
+                    domain_holder
+                        .user
+                        .controller
+                        .create_if_not_exists(reply_user, chat_id)
+                        .await?;
 
-            domain_holder
-                .user
-                .controller
-                .create_if_not_exists(reply_msg.from().unwrap(), chat_id)
-                .await?;
-
-            domain_holder
-                .user
-                .controller
-                .create_rating_record(cx, rating_trigger)
-                .await?;
+                    domain_holder
+                        .user
+                        .controller
+                        .create_rating_record(cx, rating_trigger)
+                        .await?;
+                }
             }
         }
     }
